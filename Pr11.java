9e89fdedd1aef922b2606a83d625a48ec7766bd0
nishthamendpara <nishthamendpara@gmail.com>
2026-05-08 14:15:12 +0530

    Create Pr11.java

---
+++ Pr11.java
@@ -0,0 +1,28 @@
+public class MatrixChain {
+    static int matrixChainOrder(int p[], int n) {
+        int m[][] = new int[n][n];
+
+        for (int L = 2; L < n; L++) {
+            for (int i = 1; i < n - L + 1; i++) {
+                int j = i + L - 1;
+                m[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i; k <= j - 1; k++) {
+                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+
+                    if (q < m[i][j])
+                        m[i][j] = q;
+                }
+            }
+        }
+
+        return m[1][n - 1];
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3, 4};
+
+        System.out.println("Minimum multiplications: " + matrixChainOrder(arr, arr.length));
+    }
+}
+//Minimum multiplications: 18
